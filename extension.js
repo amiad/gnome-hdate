a8fb3a474d1553c23d6921c472d29a4eeb9df909
Amiad <hatul@hatul.info>
2012-12-05 18:12:19 +0200

    add check if month or year changed

--- extension.js
+++ extension.js
@@ -2,8 +2,10 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Hdate = imports.gi.LibHdateGlib.Hdate;
+const Mainloop = imports.mainloop;
 
 let date;
+var currentDate;
 
 function init() {
     date = new St.Bin({ style_class: 'panel-date',
@@ -12,17 +14,24 @@ function init() {
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
-    var h = Hdate.new();
-    var d = new Date();
-    h.set_gdate(d.getDate(),d.getMonth()+1,d.getFullYear());
-    let label = new St.Label({ text: h.get_format_date(0,0)});
+}
 
-    date.set_child(label);
-    //date.connect('date-press-event', _showHello);
+function update(){
+    var d = new Date();
+    if( !currentDate || d.getDate()!=currentDate.getDate() || d.getMonth()!=currentDate.getMonth() || d.getFullYear()!=currentDate.getFullYear()){
+      var h = Hdate.new();
+      h.set_gdate(d.getDate(),d.getMonth()+1,d.getFullYear());
+      let label = new St.Label({ text: h.get_format_date(0,0)});
+      date.set_child(label);
+      Main.panel._centerBox.remove_child(date);
+      Main.panel._centerBox.insert_child_at_index(date, 0);    
+    }
+    Mainloop.timeout_add(60000, function () { update(); });
+    currentDate=d;
 }
 
 function enable() {
-    Main.panel._centerBox.insert_child_at_index(date, 0);
+    update();
 }
 
 function disable() {
