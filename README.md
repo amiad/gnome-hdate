5e3179907b704d1a93bec1ff054b1c5b498b3942
Amiad <hatul@hatul.info>
2012-12-09 15:49:24 +0200

    add changes by kobi zamir (libhdate-glib 0.5)

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Gnome-shell extension for show Hebrew date in the panel.
 
 ##Dependencies
 * vala
-* [libhdate](http://libhdate.sourceforge.net/) - Existing in repositories of most distributions. 
-* [libhdate-glib](http://libhdate-glib.googlecode.com/) - Arch [AUR](https://aur.archlinux.org/packages/libhdate-glib/)
+* [libhdate](http://libhdate.sourceforge.net/) - Existing in repositories of most distributions. Above version 0.2 libhadte>=1.6.
+* [libhdate-glib](http://libhdate-glib.googlecode.com/) - Arch [AUR](https://aur.archlinux.org/packages/libhdate-glib/). Above version 0.2 libhdate-glib>=0.5.
 
 ##Installition
 Rename the directory to `hdate@hatul.info` and move its to `~/.local/share/gnome-shell/extensions`.
--- extension.js
+++ extension.js
@@ -1,39 +1,75 @@
-
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Hdate = imports.gi.LibHdateGlib.Hdate;
 const Mainloop = imports.mainloop;
 
-let date;
-var currentDate;
+var date;
+var h;
+var gday;
+
+var updateTimeout = 60000;
+var isRunning = false;
+var forceHebrew = true;
 
 function init() {
     date = new St.Bin({ style_class: 'panel-date',
-                          reactive: true,
-                          can_focus: true,
+                          reactive: false,
+                          can_focus: false,
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
+    
+    // init the hebrew date object
+    h = Hdate.new();
 }
 
-function update(){
-    var d = new Date();
-    if( !currentDate || d.getDate()!=currentDate.getDate() || d.getMonth()!=currentDate.getMonth() || d.getFullYear()!=currentDate.getFullYear()){
-      var h = Hdate.new();
-      h.set_gdate(d.getDate(),d.getMonth()+1,d.getFullYear());
-      let label = new St.Label({ text: h.get_format_date(0,0)});
-      date.set_child(label);
-      Main.panel._centerBox.remove_child(date);
-      Main.panel._centerBox.insert_child_at_index(date, 0);    
+function update() {
+    // check extension state
+    if ( !isRunning ) {
+        return;
     }
-    Mainloop.timeout_add(60000, function () { update(); });
-    currentDate=d;
+    
+    // force long format hebrew output
+    h.set_use_hebrew( forceHebrew );
+    h.set_use_short_format( false );
+    
+    // set the h object date to today 
+    // and repaint if we did not paint this day
+    h.set_today();
+    if ( h.get_gday() != gday ) {
+        // create the hebrew date label. we can use the standart h.to_string()
+        // function or create the string.
+        let label_string = h.get_int_string( h.get_hday() );
+        label_string += " " + h.get_hebrew_month_string( h.get_hmonth() );
+        label_string += " " + h.get_int_string( h.get_hyear() );
+        
+        // check for holiday
+        let holyday = h.get_holyday();
+        if (holyday != 0) {
+          label_string += ", " + h.get_holyday_string( holyday );
+        }
+        
+        let label = new St.Label({ text: label_string });
+        
+        date.set_child( label );
+        Main.panel._centerBox.remove_child( date );
+        Main.panel._centerBox.insert_child_at_index( date, 0 );
+    }
+    
+    // remember the last day painted
+    gday = h.get_gday();
+    
+    // reset the timeout to 1 min from now
+    Mainloop.timeout_add( updateTimeout, function() { update(); } );
 }
 
 function enable() {
+    gday = 0;
+    isRunning = true; 
     update();
 }
 
 function disable() {
-    Main.panel._centerBox.remove_child(date);
+    isRunning = false; 
+    Main.panel._centerBox.remove_child( date );
 }
