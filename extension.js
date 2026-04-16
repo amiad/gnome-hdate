8bf3aa5d1a271843b108d09ace4c33807586ad60
amiad <arch@hatul.info>
2026-04-16 14:21:27 +0300

    add convert numbers to hebrew letters

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import St from 'gi://St';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
 import GLib from 'gi://GLib';
@@ -27,13 +27,13 @@ const HdateButton = new GObject.registerClass({
         super(0.0, "Hdate Button", false);
         this._extension = extension;
         this._settings = extension.getSettings('org.gnome.shell.extensions.hdate');
-        
+
         this.buttonText = new St.Label({
-            y_expand: true, 
+            y_expand: true,
             y_align: Clutter.ActorAlign.CENTER
         });
         this.add_child(this.buttonText);
-        
+
         this.h = new LibHdate();
         this.jd = 0;
 
@@ -42,7 +42,7 @@ const HdateButton = new GObject.registerClass({
         this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, this._refresh.bind(this));
         this._refresh();
     }
-    
+
     _applySettings() {
         this.latitude = this._settings.get_double('latitude');
         this.longitude = this._settings.get_double('longitude');
@@ -50,8 +50,8 @@ const HdateButton = new GObject.registerClass({
     }
 
     _refresh_button_label() {
-        let label = `${this.h.get_int_string(this.h.get_hday())} ב${this.h.get_hebrew_month_string(this.h.get_hmonth())} ${this.h.get_int_string(this.h.get_hyear())}`;
-        
+        let label = this.h.getFullHebrewDate();
+
         let holyday = this.h.get_holyday();
         if (holyday !== 0) {
             label += `, ${this.h.get_holyday_string(holyday)}`;
@@ -63,7 +63,7 @@ const HdateButton = new GObject.registerClass({
         }
         this.buttonText.set_text(label);
     }
-    
+
     _refresh_button_menu() {
         this.menu.removeAll();
         let tzMin = getSystemTzOffset() * 60;
@@ -72,7 +72,7 @@ const HdateButton = new GObject.registerClass({
             _("Sunrise: ") + minToString(this.h.get_sunrise(this.latitude, this.longitude) + tzMin)));
         this.menu.addMenuItem(new PopupMenu.PopupMenuItem(
             _("Sunset: ") + minToString(this.h.get_sunset(this.latitude, this.longitude) + tzMin)));
-        
+
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         let tempH = new LibHdate();
@@ -83,16 +83,16 @@ const HdateButton = new GObject.registerClass({
             tempH.set_gdate(next.get_day_of_month(), next.get_month(), next.get_year());
             tries++;
         }
-        
+
         this.menu.addMenuItem(new PopupMenu.PopupMenuItem(
             _("Week's Torah: ") + tempH.get_parasha_string(tempH.get_parasha())));
-        
+
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         let settingsItem = new PopupMenu.PopupMenuItem(_('Settings ⚙'));
         settingsItem.connect('activate', () => this._extension.openPreferences());
         this.menu.addMenuItem(settingsItem);
     }
-    
+
     _refresh(force = false) {
         this.h.set_today();
         if (force || this.h.get_julian() !== this.jd) {
